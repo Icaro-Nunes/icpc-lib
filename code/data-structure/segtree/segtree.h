573071f9ee38292e7820ce5b5c316b23ad876e50
Icaro-Nunes <icarolimanunes@gmail.com>
2024-10-02 17:59:45 -0300

    data-structure(segtree): reduce variable names and add comments

--- code/data-structure/segtree/segtree.h
+++ code/data-structure/segtree/segtree.h
@@ -10,39 +10,60 @@
 template<typename T, class Op>
 struct STree {
 	int size;
-	int treesize;
-	T* items;
-	T* nodes;
+
+	// treesize -> smallest power
+  	// of 2 that is >= size
+  	// (the actual space taken by
+  	// the tree is 2*treesize)
+	int ts; 
+
+	// items -> pointer to the 
+  	// start of the leaves of
+  	// the segtree (implemented
+  	// as a perfect binary tree)
+	T* it;
+
+	// nodes -> pointer to the 
+  	// whole tree, with an extra
+  	// unused node (nd[0])
+	T* nd;
+
+	// operation struct, must be
+  	// implemented as:
+  	// struct Op {
+  	//  T neutral;
+  	//  T operator()(const T& a, const T& b) {}
+  	// }
 	Op op;
 
 	STree(const std::vector<T>& from) {
-		treesize=1;
+		ts=1;
 
-		while(treesize < from.size())
-			treesize <<=1;
+		while(ts < from.size())
+			ts <<=1;
 
 		this->size = from.size();
-		nodes = new T[2*treesize];
-		items = nodes+treesize;
+		nd = new T[2*ts];
+		it = nd+ts;
 
-		for(int i=0; i<2*treesize; i++)
-			nodes[i] = op.neutral;
+		for(int i=0; i<2*ts; i++)
+			nd[i] = op.neutral;
 
 		for(int i=0; i<size; i++)
-			items[i] = from[i];
+			it[i] = from[i];
 
-		for(int i=2*treesize-1; i>1; i--)
-			nodes[i>>1] = op(nodes[i], nodes[i>>1]);
+		for(int i=2*ts-1; i>1; i--)
+			nd[i>>1] = op(nd[i], nd[i>>1]);
 	}
 
-	void update(int index, T value) {
-		int i = treesize+index;
+	void update(int idx, T val) {
+		int i = ts+idx;
 
-		nodes[i] = value;
+		nd[i] = val;
 		i = i>>1;
 
 		while(i>0) {
-			nodes[i] = op(nodes[2*i], nodes[2*i+1]);
+			nd[i] = op(nd[i>>1], nd[(i>>1)+1]);
 			i>>=1;
 		}
 	}
@@ -53,40 +74,41 @@ struct STree {
 		
 		r++;
 
-		for(l += treesize, r += treesize; l<r; l>>=1, r>>=1) {
-			if(l&1) resl = op(resl, nodes[l++]);
-			if(r&1) resr = op(nodes[--r], resr);
+		for(l += ts, r += ts; l<r; l>>=1, r>>=1) {
+			if(l&1) resl = op(resl, nd[l++]);
+			if(r&1) resr = op(nd[--r], resr);
 		}
 
 		return op(resl, resr);
 	}
 
 	~STree() {
-		delete nodes;
+		delete nd;
 	}
 
 	// search for first ocurrence to the right of strt
 	// of a value greater than or equal to v
-  	// REQUIRES TESTING ON STRT != 0
 	int lower_boundr(int strt, T v) {
-		int i=treesize+strt;
-		T accum=nodes[i];
+		int i=ts+strt;
 
-		while(i>1 and !(accum >= v)) {
-			if(!(i&1) and op(accum, nodes[i+1])>=v) break;
-			if(!(i&1)) accum = op(accum, nodes[i+1]);
+		// accumulated value
+		T acc=nd[i];
+
+		while(i>1 and !(acc >= v)) {
+			if(!(i&1) and op(acc, nd[i+1])>=v) break;
+			if(!(i&1)) acc = op(acc, nd[i+1]);
 			i>>=1;
 		}
 
 		if(i==1) return size;
-		if(!(accum >= v)) i++;
+		if(!(acc >= v)) i++;
 
-		while(i<treesize) {
-			if(op(accum, nodes[i<<1])>=v) i <<= 1;
-			else accum = op(accum, nodes[i<<1]), i = (i<<1) + 1;
+		while(i<ts) {
+			if(op(acc, nd[i<<1])>=v) i <<= 1;
+			else acc = op(acc, nd[i<<1]), i = (i<<1) + 1;
 		}
 
-		return i-treesize;
+		return i-ts;
 	}
 };
 
